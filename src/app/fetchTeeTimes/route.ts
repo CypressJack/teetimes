af94e8fe1a414366dc93801630c0f76c6568d18f
Wade Croft <wadecroft@gmail.com>
2024-03-21 12:27:57 -0700

    update route to fetch one date

--- src/app/fetchTeeTimes/route.ts
+++ src/app/fetchTeeTimes/route.ts
@@ -2,72 +2,60 @@ export const dynamic = 'force-dynamic' // defaults to auto
 import { getNextSixDaysPST } from '@/utils/dates';
 import { TeeTime } from '../types';
 import { prisma } from "@/lib/db";
-import { headers } from 'next/headers'
 
 export async function GET(request: Request) {
-  const headersList = headers();
-
-  const days = getNextSixDaysPST();
-
-  for (const day of days) {
-    await fetchData(day);
+  const { searchParams } = new URL(request.url)
+  const date = searchParams.get('date');
+  if (date) {
+    await fetchData(date);
+    return Response.json({ status: "ok" })
   }
-  
-  return Response.json({ status: "ok" })
+  else {
+    return Response.json({ status: "no date provided" })
+  }
 }
 
+// async function fetchOneWeek() {
+//   const days = getNextSixDaysPST();
+
+//   for (const day of days) {
+//     setTimeout(async () => {
+//       await fetchData(day);
+//     }, 5000);
+//   }
+// }
+
 async function fetchData(date: string) {
-  try {
-    // Fetching tee times for four players
-    const response = await fetch(`https://www.chronogolf.com/marketplace/clubs/593/teetimes?date=${date}&course_id=573&affiliation_type_ids%5B%5D=3089&affiliation_type_ids%5B%5D=3089&affiliation_type_ids%5B%5D=3089&affiliation_type_ids%5B%5D=3089`);
+  // Fetching tee times for four players
+  const response = await fetch(`https://www.chronogolf.com/marketplace/clubs/593/teetimes?date=${date}&course_id=573&affiliation_type_ids%5B%5D=3089&affiliation_type_ids%5B%5D=3089&affiliation_type_ids%5B%5D=3089&affiliation_type_ids%5B%5D=3089`);
 
-    const teeTimes: TeeTime[] = await response.json();
+  const teeTimes: TeeTime[] = await response.json();
 
-    for (let teeTime of teeTimes) {
-      const id = teeTime.id;
-      const openSpots = teeTime.out_of_capacity ? 0 : 4;
-      const dbTeeTime = await prisma.teeTime.findUnique(
-        {
-          where: { id: id }
-        }
-      )
+  for (let teeTime of teeTimes) {
+    const id = teeTime.id;
+    const openSpots = teeTime.out_of_capacity ? 0 : 4;
 
-      if (dbTeeTime) {
-        if (dbTeeTime.out_of_capacity === false && teeTime.out_of_capacity === true) {
-          // do something when it has changed
-        }
-
-
-        // update existing teetime
-        await prisma.teeTime.update({
-          where: { id },
-          data: {
-            date: teeTime.date,
-            start_time: teeTime.start_time,
-            open_spots: openSpots,
-            out_of_capacity: teeTime.out_of_capacity,
-            green_fee: teeTime.green_fees[0].price
-          }
-        })
+    // update existing teetime
+    await prisma.teeTime.upsert({
+      where: { id },
+      update: {
+        date: teeTime.date,
+        start_time: teeTime.start_time,
+        open_spots: openSpots,
+        out_of_capacity: teeTime.out_of_capacity,
+        green_fee: teeTime.green_fees[0].price
+      },
+      create: {
+        id: teeTime.id,
+        date: teeTime.date,
+        start_time: teeTime.start_time,
+        open_spots: openSpots,
+        out_of_capacity: teeTime.out_of_capacity,
+        green_fee: teeTime.green_fees[0].price
       }
+    })
 
-      // create new teetime
-      if (!dbTeeTime) {
-        await prisma.teeTime.create({
-          data: {
-            id: teeTime.id,
-            date: teeTime.date,
-            start_time: teeTime.start_time,
-            open_spots: openSpots,
-            out_of_capacity: teeTime.out_of_capacity,
-            green_fee: teeTime.green_fees[0].price
-          }
-        })
-      }
-    }
-    console.log('successfully fetched');
-
-  } catch (error) {
-    console.error('Error fetching or saving data:', error);
   }
+  console.log('successfully fetched');
+
 }
